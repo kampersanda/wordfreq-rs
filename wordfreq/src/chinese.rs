f861228ed84cb22cd30d557e69fa9006d2bd3aa8
kampersanda <shnsk.knd@gmail.com>
2023-06-10 11:08:46 +0900

    add

---
+++ wordfreq/src/chinese.rs
@@ -0,0 +1,1 @@
+// https://github.com/rspeer/wordfreq/blob/v3.0.2/wordfreq/chinese.py
--- wordfreq/src/lib.rs
+++ wordfreq/src/lib.rs
@@ -61,6 +61,7 @@
 //! ```
 #![deny(missing_docs)]
 
+mod chinese;
 pub mod language;
 mod numbers;
 pub mod preprocessers;
